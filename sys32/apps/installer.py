45c26cec1fb2c40c758f6202b810bb9599421594
Gamer000gaming <154331650+Gamer000gaming@users.noreply.github.com>
2023-12-19 17:45:15 +0100

    Update installer.py

--- sys32/apps/installer.py
+++ sys32/apps/installer.py
@@ -2,7 +2,8 @@ import tkinter as tk
 import shutil
 import os
 
-def installer(path):
+def installer():
+    path = tk.filedialog.askdirectory()
     shutil.copy(path, os.path.dirname(__file__))
 
 installer(r"C:\Users\Jules07\OneDrive\Bureau\python\convert.py")
